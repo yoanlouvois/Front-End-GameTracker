225719b69e8eef1772448dc8742bcb19c333c9c3
Valentin <valentin.rontard@yahoo.fr>
2025-03-27 10:03:09 +0100

    acceuil

--- src/app/Ecrans/ecran-connexion/ecran-connexion.component.ts
+++ src/app/Ecrans/ecran-connexion/ecran-connexion.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatIcon} from '@angular/material/icon';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {NgIf} from '@angular/common';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-ecran-connexion',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    MatIcon,
+    MatLabel,
+    MatFormField,
+    MatIconButton,
+    MatInput,
+    MatButton,
+    NgIf,
+  ],
   templateUrl: './ecran-connexion.component.html',
   styleUrl: './ecran-connexion.component.scss'
 })
 export class EcranConnexionComponent {
-
+  username: any;
+  password: any;
+  constructor(private router: Router) { }
+  goToAccueil() {
+    this.router.navigate(['/Home']);
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {HeaderComponent} from './Components/header/header.component';
 import {LayoutComponent} from './Components/layout/layout.component';
+import {EcranConnexionComponent} from './Ecrans/ecran-connexion/ecran-connexion.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,  // Composant standalone
-  imports: [RouterModule, LayoutComponent],  // Importer le module de routage et le composant HelloPage
+  imports: [RouterModule, LayoutComponent, EcranConnexionComponent],  // Importer le module de routage et le composant HelloPage
 })
 export class AppComponent {
   title = 'Mon Application';
