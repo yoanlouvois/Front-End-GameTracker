7f2023472b27e3633bb39433385bc335157980d7
Yoan <louvois45@gmail.com>
2025-04-02 19:24:38 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/app/Ecrans/ecran-profil/ecran-profil.component.html

--- src/app/Ecrans/ecran-inscription/ecran-inscription.component.ts
+++ src/app/Ecrans/ecran-inscription/ecran-inscription.component.ts
@@ -1,40 +1,40 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
-import {NgIf} from "@angular/common";
-import {MatIcon} from '@angular/material/icon';
-import {MatButton, MatIconButton} from '@angular/material/button';
-import {Router} from '@angular/router';
-import {HttpClient} from '@angular/common/http';
-import {UserDto} from '../../services/models/user-dto';
-import {createUser} from '../../services/fn/user-controller/create-user';
-import {AuthenticationControllerService} from '../../services/services/authentication-controller.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { UserDto } from '../../services/models/user-dto';
+import { createUser } from '../../services/fn/user-controller/create-user';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+// Importez les modules complets
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; // Important!
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-ecran-inscription',
-    imports: [
-      ReactiveFormsModule,
-      FormsModule,
-      MatIcon,
-      MatLabel,
-      MatFormField,
-      MatIconButton,
-      MatInput,
-      MatButton,
-      NgIf,
-    ],
   templateUrl: './ecran-inscription.component.html',
-  styleUrl: './ecran-inscription.component.scss'
+  standalone: true, // Définir à true pour utiliser imports au niveau du composant
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule, // Nécessaire pour matInput
+    MatButtonModule // Pour les boutons mat-raised-button
+  ],
+  styleUrls: ['./ecran-inscription.component.scss']
 })
 export class EcranInscriptionComponent {
-  // Déclaration du formulaire
   username: string = '';
   email: string = '';
   password: string = '';
 
-  constructor(private router: Router, private authService: AuthenticationControllerService) {}
-/*
-  // Quand l'utilisateur clique sur Valider
+  // Déclarez les dépendances nécessaires
+  constructor(private http: HttpClient, private router: Router) {}
+
+  // Méthode d'inscription
   createUser() {
     if (!this.username || !this.email || !this.password) {
       alert('Tous les champs sont obligatoires');
@@ -47,22 +47,29 @@ export class EcranInscriptionComponent {
       password: this.password,
     };
 
-    // Appeler la méthode createUser depuis le service
-    this.authService
-      .createUser({ body: newUser }) // Fournissez le paramètre attendu
+    // Appel direct de la méthode `createUser`
+    createUser(this.http, 'http://localhost:8081', { body: newUser })
       .subscribe({
-        next: (res) => {
-          console.log('Utilisateur créé avec succès :', res);
+        next: (response) => {
+          console.log('Utilisateur créé avec succès :', response);
           alert('Inscription réussie ! Vous allez être redirigé vers la page de connexion.');
-          this.router.navigate(['/Connexion']); // Rediriger vers la page de connexion
+          this.router.navigate(['/Connexion']);
         },
         error: (err) => {
           console.error('Erreur lors de la création de l\'utilisateur :', err);
-          alert('Une erreur est survenue lors de l\'inscription. Veuillez réessayer.');
+
+          if (err.status === 400) {
+            alert('Champs invalides. Veuillez vérifier les informations saisies.');
+          } else if (err.status === 500) {
+            alert('Erreur serveur. Veuillez réessayer plus tard.');
+          } else {
+            alert('Une erreur est survenue. Veuillez réessayer.');
+          }
         },
       });
-  }*/
+  }
 
+  // Navigation vers d'autres pages
   goToConnexion() {
     this.router.navigate(['/Connexion']);
   }
