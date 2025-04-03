f43d4de1cdc27af821a42423a187226444825ab8
Yoan <louvois45@gmail.com>
2025-04-03 13:16:08 +0200

    Initialisation de la base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {HeaderComponent} from './Components/header/header.component';
 import {LayoutComponent} from './Components/layout/layout.component';
 import {EcranConnexionComponent} from './Ecrans/ecran-connexion/ecran-connexion.component';
+import { DbInitializerService } from './services/db-initializer.service'
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,14 @@ import {EcranConnexionComponent} from './Ecrans/ecran-connexion/ecran-connexion.
   standalone: true,  // Composant standalone
   imports: [RouterModule, LayoutComponent],  // Importer le module de routage et le composant HelloPage
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Mon Application';
+  constructor(private dbInitializer: DbInitializerService) {}
+
+  ngOnInit() {
+    // Initialiser la base de données au démarrage de l'application
+    this.dbInitializer.initializeGamesIfEmpty()
+      .then(() => console.log('Vérification/initialisation de la base de données terminée.'))
+      .catch(error => console.error('Erreur lors de l\'initialisation de la base de données:', error));
+  }
 }
